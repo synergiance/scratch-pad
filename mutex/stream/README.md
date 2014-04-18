0ca515ca8e74743b4d0ab80f6c0dd547018a6203
synergiance <lordmicro@gmail.com>
2014-04-18 03:46:47 -0400

    Safe Cout

---
+++ mutex/stream/README.md
@@ -0,0 +1,3 @@
+## Safe Stream
+
+This function will supposedly be used as a buffer with mutex lock which prevents accidental data corruption in multithreaded applications
---
+++ mutex/stream/main.cpp
@@ -0,0 +1,54 @@
+/*
+ * main.cpp
+ *
+ *  Created on 18 April 2014
+ *      By: Synergiance
+ */
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
+bool ready = false;
+
+void scout(ostream& stream, const char* s)
+{
+	mtx.lock();
+	cout(stream, s);
+	mtx.unlock();
+}
+
+void showID(int ID)
+{
+    unique_lock<mutex> lck(mtx);
+	while (!ready) cv.wait(lck);
+	// 3 2 1 GO!
+	scout<<"Thread #"<<ID<<endl;
+}
+
+void go() {
+    unique_lock<mutex> lck(mtx);
+    ready = true;
+    cv.notify_all();
+}
+
+int main ()
+{
+    std::thread threads[10];
+    // spawn 20 threads:
+    for (int i=0; i<20; ++i)
+        threads[i] = thread(showID,i);
+
+    cout << "10 threads ready to race...\n";
+    go();                       // go!
+
+    for (auto& th : threads) th.join();
+
+    return 0;
+}
+
