20d4893e8206d8ffab47e6c4da40c54a61a4f15a
synergiance <lordmicro@gmail.com>
2014-04-21 19:54:38 -0400

    Add terminal test header
    This is the beginning of a project that will allow one to type on
    the terminal to the program while having lines output to the screen
    without taking over the last line

---
+++ sockets/select/stdin/cterm.cpp
@@ -0,0 +1,12 @@
+/*
+ * cterm.cpp
+ *
+ *  Created on 21 April 2014
+ *      By: Tim Gerety
+ */
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
